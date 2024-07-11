9652d472048880911f3d52fb3cee303b44e19f86
j0k3r-rg <pdasriogallegos@gmail.com>
2024-07-11 01:24:54 -0300

    actualizacion para app

--- src/main/java/com/j0k3r/andreanamaste/AndreanamasteApplication.java
+++ src/main/java/com/j0k3r/andreanamaste/AndreanamasteApplication.java
@@ -1,7 +1,9 @@
 package com.j0k3r.andreanamaste;
 
 import com.j0k3r.andreanamaste.models.Product;
+import com.j0k3r.andreanamaste.models.ProfileItem;
 import com.j0k3r.andreanamaste.repositories.ProductRepository;
+import com.j0k3r.andreanamaste.repositories.ProfileItemRepository;
 import com.j0k3r.andreanamaste.security.enums.Role;
 import com.j0k3r.andreanamaste.security.models.User;
 import com.j0k3r.andreanamaste.security.repository.UserRepository;
@@ -28,6 +30,9 @@ public class AndreanamasteApplication implements CommandLineRunner {
 	@Autowired
 	private ProductRepository productRepository;
 
+	@Autowired
+	private ProfileItemRepository profileItemRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 		userRepository.save(
@@ -39,15 +44,83 @@ public class AndreanamasteApplication implements CommandLineRunner {
 						.enable(true).build()
 		);
 
-		List.of(1,2,3,4,5,6).forEach(
-				i -> productRepository.save(
-						Product.builder()
-								.name("Producto "+i)
-								.description("Descripcion del producto "+i)
-								.priceARS(Double.valueOf(i))
-								.priceUSD(Double.valueOf(i))
-								.build()
-		));
+		userRepository.save(
+				User.builder()
+						.username("user")
+						.password(new BCryptPasswordEncoder().encode("user"))
+						.role(Role.ROLE_USER)
+						.email("user@mail.com")
+						.enable(true).build()
+		);
+
+		productRepository.save(
+			Product.builder()
+				.name("Decodificacion Arquetipal")
+				.description("La decodificación arquetípica es un método de análisis personal que se basa en la psicología junguiana para identificar los arquetipos que influyen en nuestro comportamiento, pensamientos y emociones.")
+				.priceARS(15000.0)
+				.priceUSD(10.0)
+				.imageUrl("https://i.imgur.com/mtlae37.jpg")
+				.build()
+		);
+
+		productRepository.save(
+				Product.builder()
+						.name("Feng Shui")
+						.description("Armoniza tu entorno y potencia tu bienestar con Feng Shui. Experto te guía para optimizar la energía de tu hogar o negocio.")
+						.priceARS(15000.0)
+						.priceUSD(10.0)
+						.imageUrl("https://i.imgur.com/0rhoE4E.jpg")
+						.build()
+		);
+
+		productRepository.save(
+				Product.builder()
+						.name("Pendulo Egreo de Orgon")
+						.description("Equilibra tu energía y bienestar. Armoniza tu aura y elimina bloqueos energéticos con la sabiduría ancestral del Pendulo Egreo de Orgon.")
+						.priceARS(15000.0)
+						.priceUSD(10.0)
+						.imageUrl("https://i.imgur.com/sfcP1Ex.jpg")
+						.build()
+		);
+
+		productRepository.save(
+				Product.builder()
+						.name("Sanacion pranica")
+						.description("Sana cuerpo, mente y espíritu. Activa tu energía vital y recupera tu equilibrio natural con la milenaria técnica de Sanación Pránica.")
+						.priceARS(15000.0)
+						.priceUSD(10.0)
+						.imageUrl("https://i.imgur.com/XMr6H5R.jpg")
+						.build()
+		);
+
+		profileItemRepository.save(
+				ProfileItem.builder()
+						.name("about_me")
+						.body("Soy Andrea Namaste, terapeuta holística y profesora de yoga. Mi misión es acompañarte en tu proceso de sanación y transformación personal, para que puedas conectar con tu esencia y vivir en armonía con tu cuerpo, mente y espíritu.")
+						.build()
+		);
+
+		profileItemRepository.save(
+				ProfileItem.builder()
+						.name("image_about_me")
+						.name("image_about_me")
+						.body("https://i.imgur.com/j1OA1US.jpeg")
+						.build()
+		);
+
+		profileItemRepository.save(
+				ProfileItem.builder()
+						.name("text_motivation")
+						.body("El camino no esta en el cielo, el camino esta en el corazon")
+						.build()
+		);
+
+		profileItemRepository.save(
+				ProfileItem.builder()
+						.name("author_text_motivation")
+						.body("Buda")
+						.build()
+		);
 
 	}
 }
