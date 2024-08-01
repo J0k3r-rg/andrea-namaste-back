58aa82a9cc6c5f0a131cc058fa8e0fc15dd0b47e
j0k3r-rg <pdasriogallegos@gmail.com>
2024-08-01 17:27:35 -0300

    corrections query reservations

--- src/main/java/com/j0k3r/andreanamaste/repositories/ReservationRepository.java
+++ src/main/java/com/j0k3r/andreanamaste/repositories/ReservationRepository.java
@@ -2,6 +2,8 @@ package com.j0k3r.andreanamaste.repositories;
 
 import com.j0k3r.andreanamaste.models.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,5 +11,11 @@ public interface ReservationRepository extends JpaRepository<Reservation, String
 
     List<Reservation> findByUserId(String id);
 
-    Boolean existsByIsPaidOrFinallyShiftAndUserId(Boolean isPaid, Boolean finallyShift, String userId);
+    @Query("SELECT CASE WHEN COUNT(r) > 0 THEN TRUE ELSE FALSE END FROM " +
+            "Reservation r WHERE (r.isPaid = :isPaid OR r.finallyShift = :finallyShift) " +
+            "AND r.user.id = :userId")
+    Boolean existsByIsPaidOrFinallyShiftAndUserId(
+            @Param("isPaid") Boolean isPaid,
+            @Param("finallyShift") Boolean finallyShift,
+            @Param("userId") String userId);
 }
